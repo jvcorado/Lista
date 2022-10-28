907cfd2f21f516991d1d4bdb77dcc99a56acfbb7
João Victor Corado <jhonisilva545@gmail.com>
2022-10-28 14:45:26 -0300

    Update acesso.js

--- js/acesso.js
+++ js/acesso.js
@@ -3,7 +3,7 @@ const senhaInput = document.querySelector("#senha");
 
 function logar(){
     if(userInput.value == 'admin' && senhaInput.value == 'admin'){
-        return window.location.href='index.html'
+        return window.location.href='postagem.html'
         
     }
     else{
